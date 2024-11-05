ee7fbbf8d2fa5a7442a356744023d719affc49a1
Aviral Jain <avi.aviral140@gmail.com>
2024-11-05 16:56:41 +0530

    add /api/v1 prefix to urls

--- handlers/routes.go
+++ handlers/routes.go
@@ -2,15 +2,16 @@ package handlers
 
 func (s *Server) InitRoutes() {
 	s.Router.HandleFunc("/", s.Ping).Methods("GET")
+	v1Router := s.Router.PathPrefix("/api/v1").Subrouter()
 
-	threadRouter := s.Router.PathPrefix("/thread").Subrouter()
+	threadRouter := v1Router.PathPrefix("/thread").Subrouter()
 	threadRouter.HandleFunc("", s.ListThreads).Methods("GET")
 	threadRouter.HandleFunc("", s.CreateThread).Methods("POST")
 	threadRouter.HandleFunc("/{id}", s.GetThread).Methods("GET")
 	threadRouter.HandleFunc("/{id}", s.UpdateThread).Methods("PUT")
 	threadRouter.HandleFunc("/{id}", s.DeleteThread).Methods("DELETE")
 
-	messageRouter := s.Router.PathPrefix("/message").Subrouter()
+	messageRouter := v1Router.PathPrefix("/message").Subrouter()
 	messageRouter.HandleFunc("/{id}", s.GetMessage).Methods("GET")
 	messageRouter.HandleFunc("/{id}", s.UpdateMessage).Methods("PUT")
 	messageRouter.HandleFunc("/{id}", s.DeleteMessage).Methods("DELETE")
