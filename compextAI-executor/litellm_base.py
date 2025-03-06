7f979a4de9d7ddf4c49e3509cf29f13d04161f2c
rpatil1998 <rpatil@ec.iitr.ac.in>
2025-03-06 15:56:47 +0530

    added vertex provider for claude-3-7

--- compextAI-executor/litellm_base.py
+++ compextAI-executor/litellm_base.py
@@ -80,6 +80,14 @@ def get_model_list(api_keys:dict):
             "api_key": api_keys.get("anthropic", "")
         }
     },
+    {
+        #https://console.cloud.google.com/vertex-ai/publishers/anthropic/model-garden/claude-3-7-sonnet?hl=en&project=dashwave
+        "model_name": "claude-3-7-sonnet",
+        "litellm_params": {
+            "model": "vertex_ai/claude-3-7-sonnet@20250219",
+            "vertex_credentials": json.dumps(api_keys.get("google_service_account_creds", {})),
+        }
+    },
     ]
 
 def get_model_identifier(model_name:str):
